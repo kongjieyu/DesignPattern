fc931b219a1a204d3b4a2b1592fa47648e12ecee
May Kong <1285235080@qq.com>
2019-01-08 21:13:18 +1300

    inheritance

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-class Person{
+//父类
+class People{
     constructor(name,age){
         this.name = name
         this.age = age
@@ -11,10 +12,31 @@ class Person{
     }
 }
 
-let Kong = new Person('May', 18)
-Kong.eat()
-Kong.speak()
+// let Kong = new People('May', 18)
+// Kong.eat()
+// Kong.speak()
 
-let Chen = new Person('Chen', 25)
-Chen.eat()
-Chen.speak()
+// let Chen = new People('Chen', 25)
+// Chen.eat()
+// Chen.speak()
+//------------------
+
+//子类： 继承父类
+class Student extends People {
+    constructor(name,age,number){
+        super(name,age)
+        this.number = number
+    }
+    study() {
+        alert(`${this.name} studys`)
+    }
+}
+
+let xiaoming = new Student('xiaoming', 18, 'A1')
+xiaoming.study()
+alert(xiaoming.number)
+xiaoming.eat()
+let xiaochen = new Student('xiaochen', 28, 'A2')
+xiaochen.speak()
+xiaochen.study()
+alert(xiaochen.number)
