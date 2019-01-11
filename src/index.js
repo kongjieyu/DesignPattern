fba6b57a1936f710782d0eb18e10a5656c880824
May Kong <1285235080@qq.com>
2019-01-11 14:27:28 +1300

    FactoryPattern & Singleton Pattern

--- src/index.js
+++ src/index.js
@@ -1,120 +1,45 @@
-//CarPark
-class Park{
-    constructor(floors){
-        //注意： 这里的constructor中没有吧camera, screen, carList作为参数传入
-        this.floors = floors || []
-        this.camera = new Camera()
-        this.screen = new Screen()
-        this.carList = {} //存储摄像头拍摄返回的车辆信息
-    }
-    in(car){
-        //通过摄像头获取信息
-        const info = this.camera.shot(car)
-        const i = parseInt(Math.random()*100)
-        const place = this.floors[0].places[i]
-        place.in()
-        info.place = place
-        //记录信息
-        this.carList[car.num] = info
-    }
-    out(car){
-       const info = this.carList[car.num]
-       const place = info.place
-       place.out()
-       //显示时间
-       this.screen.show(car, info.inTime)
-       delete this.carList[car.num]
-    }
-    emptyNum(){
-        return this.floors.map(floor=>{
-            return `${floor.index} 还有 ${floor.emptyPlaceNum()}个空余车位`
-        }).join('\n')
-    }
-}
-
-//Car
-class Car{
-    constructor(num){
-        this.num = num
-    }
-}
-
-//Floor
-class Floor{
-    //index是第几层的意思
-    constructor(index, places){
-        this.index = index
-        this.places = places || []
-    }
-    emptyPlaceNum(){
-        let num = 0
-        this.places.forEach(p=>{
-            if(p.empty){
-                num = num+1
-            }
-        })
-        return num
-    }
-}
-
-//Place
-class Place{
+class LoginForm {
     constructor(){
-        this.empty = true
+        this.state = 'hide'
     }
-    in(){
-        this.empty = false
-    }
-    out(){
-        this.empty = true
-    }
-}
-
-class Camera{
-    shot(car){
-        return{
-            num: car.num,
-            inTime: Date.now()
+    show() {
+        if(this.state === 'show'){
+            alert('已经显示')
+            return null
         }
+        this.state = 'show'
+        console.log('登陆框显示成功')
+
     }
+    hide(){
+        if(this.state === 'hide'){
+            alert('已经隐藏')
+            return null
+        }
+        this.state = 'hide'
+         console.log('登陆框已经隐藏')
+    }
+}
+var a = function(){
+
 }
 
-class Screen {
-    show(car, inTime){
-        console.log('车牌号', car.num)
-        console.log('停车时间', Date.now() - inTime)
+//自定义函数+闭包
+LoginForm.getInstance = (function(){
+    let instance
+    //闭包
+    return function(){
+        if(!instance){
+            instance = new LoginForm()
+        }
+        return instance
     }
-}
+})()
 
 //测试
-//初始化停车场
-const floors = []
-for (let i = 0; i < 3; i++) {
-    const places = []
-    for (let j =0; j<100; j++){
-        places[j] = new Place()
-    }
-    floors[i] = new Floor(i+1, places)
-}
-const park = new Park(floors)
-
-//初始化车辆
-const car1 = new Car(100)
-const car2 = new Car(200)
-const car3 = new Car(300)
-
-console.log('第一辆车进入')
-console.log(park.emptyNum())
-park.in(car1)
-console.log('第二辆车进入')
-console.log(park.emptyNum())
-park.in(car2)
-
-console.log('第一辆车离开')
-park.out(car1)
-console.log('第二辆车离开')
-park.out(car2)
-
-console.log('第三辆车进入')
-console.log(park.emptyNum())
-park.in(car3)
+let login1 = LoginForm.getInstance()
+login1.hide()
+login1.show()
+let login2 = LoginForm.getInstance()
+login2.show()
+console.log('login1===login2', login1===login2)
